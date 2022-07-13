81429f8fbb0783a3603602afd0c00b468777b592
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-07-14 00:24:47 +0100

    while loop with break and continue

--- input-while.py
+++ input-while.py
@@ -9,6 +9,13 @@
 #     message = input(prompt)
 #     print(message)
 
+
+# You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+# [7, 1]  =>  [1, 7]
+# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+# Solution
 def sort_array(arr):
     odds = sorted((x for x in arr if x % 2 != 0), reverse=True)
     print(odds)
@@ -18,3 +25,42 @@ def sort_array(arr):
 sorted_bro = sort_array([3, 8, 2, 7, 9])
 print(sorted_bro)
 
+active = True
+prompt = "Enter a cool city "
+
+# while active:
+#     message = input(prompt)
+
+#     if message == "quit":
+#         active = False
+#         print("Program ended")
+#     else:
+#         print(message)
+
+# Break example
+# while True:
+#     city = input(prompt)
+
+#     if city == "quit":
+#         break
+#     else:
+#         print(f"I would love to visit {city.title()}!")
+
+# Continue example
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     if current_number % 2 == 0:
+#         continue
+#     print(current_number)
+
+
+while True:
+    age = int(input("Enter your age: "))
+    if age < 3:
+        print("Your ticket is free")
+    elif age < 12:
+        print("Your ticket fee is $10")
+    else:
+        print("Your ticket fee is $15")
+    break
