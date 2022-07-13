438ce02f4e9d1e414fc77635cff24ff6a646e798
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-07-14 00:42:08 +0100

    filling a dictionary with user input

--- input-while.py
+++ input-while.py
@@ -55,12 +55,52 @@ prompt = "Enter a cool city "
 #     print(current_number)
 
 
-while True:
-    age = int(input("Enter your age: "))
-    if age < 3:
-        print("Your ticket is free")
-    elif age < 12:
-        print("Your ticket fee is $10")
-    else:
-        print("Your ticket fee is $15")
-    break
+# Exercise from texbook
+# while True:
+#     age = int(input("Enter your age: "))
+#     if age < 3:
+#         print("Your ticket is free")
+#     elif age < 12:
+#         print("Your ticket fee is $10")
+#     else:
+#         print("Your ticket fee is $15")
+#     break
+
+# Adding from a list to anotheer list
+# unconfirmed_users = ['alice', "brian", "candance"]
+# confirmed_users = []
+
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+
+#     print(f"Verifying user: {current_user.title()}")
+#     confirmed_users.append(current_user)
+
+# # Display all confirmed users
+# print(f"\nThe following users have been confirmed: ")
+# for confirmed_user in confirmed_users:
+#     print(confirmed_user.title())
+
+
+# FILLING A DICTIONARY WITH USER INPUT
+responses = {}
+
+# Set a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+    # Store the response in the dictionary.
+    responses[name] = response
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        print(responses)
+        polling_active = False
+
+# Polling is complete. Show the results.
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name} would like to climb {response}.")
