e88bb78e9a53b9508e8de291867594cd428c09f8
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-07-14 13:36:10 +0100

    mixingg positional and arbitrary arguments

--- function.py
+++ function.py
@@ -11,31 +11,45 @@ def describe_pet(animal_type, pet_name="Harry"):
 
 
 # Calling the function with keyword
-describe_pet(animal_type="rabbit")
-describe_pet("dog")
-describe_pet(pet_name="Josh", animal_type="Cat")
+# describe_pet(animal_type="rabbit")
+# describe_pet("dog")
+# describe_pet(pet_name="Josh", animal_type="Cat")
 
 
-def get_formatted_name(first_name, last_name, middle_name=''):
-    if middle_name:
-        full_name = f"{first_name} {middle_name} {last_name}"
-    else:
-        full_name = f"{first_name} {last_name}"
-    return full_name.title()
+# def get_formatted_name(first_name, last_name, middle_name=''):
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name.title()
 
 
-musician = get_formatted_name('jimi', 'hendrix')
-print(musician)
-musician = get_formatted_name('john', 'hooker', 'lee')
-print(musician)
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
+# musician = get_formatted_name('john', 'hooker', 'lee')
+# print(musician)
 
 
-def make_pizza(*toppings):
-    """Print the list of toppings that have been requested."""
-    print(type(toppings))
-    print(toppings.count('mushrooms'))
-    print(toppings)
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested."""
+#     print(type(toppings))
+#     print(toppings.count('mushrooms'))
+#     print('pepperoni' in toppings)
+#     print(toppings)
+
+#     # return
 
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'extra cheese', 'green peppers', 'extra cheese')
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'extra cheese',
+#            'green peppers', 'extra cheese')
+
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
