e4b8ce79e43bf6ae248934d9a5e39e6ced949c89
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-07-22 00:37:36 +0100

    instances as attributes

--- class.py
+++ class.py
@@ -16,7 +16,7 @@ class Dog:
         print(f"{self.name} rolled over!")
 
 
-my_dog = Dog("wille", 7)
+my_dog = Dog(age=7, name="Victony")
 my_dog.sit()
 
 
@@ -55,6 +55,91 @@ class Car:
         return long_name.title()
 
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
-print(type(my_new_car.get_descriptive_name()))
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
+
+
+class ElectricCar(Car):
+    """A simple Electric car class inheirted from Car class"""
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        return "this is from the electric car class"
+
+
+# my_new_car = Car('audi', 'a4', 2019)
+# print(my_new_car.get_descriptive_name())
+# print(type(my_new_car.get_descriptive_name()))
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+print(my_tesla.battery.describe_battery())
+print(my_tesla.battery.battery_size)
+print(my_tesla.battery.get_range())
+# Overriding method from parent class - DONE
+# Instance as Attributes - DONE
+
+
+# class Animal(object):
+#     """A simple Animal class"""
+
+#     def __init__(self):
+#         print("Instantiating animal class")
+
+#     def eat(self):
+#         print("I am eating")
+
+
+# class Cat(Animal):
+#     """Extending the Animal class"""
+
+#     def __init__(self):
+#         Animal.__init__(self)
+#         print("cat created")
+
+#     def meow(self):
+#         Animal.eat(self)
+#         print("meow")
+
+
+# my_cat = Cat()
+# my_cat.meow()
+# my_cat.eat()
+
+
+# import { useEffect } from 'react'
+
+# export default function useClickOutside({ ref, callback }) {
+#   useEffect(() => {
+#     const handleClickOutside = (e) => {
+#       if (ref.current && !ref.current.contains(e.target)) {
+#         callback()
+#       }
+#     }
+#     document.addEventListener('mousedown', handleClickOutside)
+#     return () => {
+#       document.removeEventListener('mousedown', handleClickOutside)
+#     }
+#   }, [ref, callback])
+# }
